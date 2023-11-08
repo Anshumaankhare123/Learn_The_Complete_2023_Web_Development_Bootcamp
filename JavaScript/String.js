72a10fa6302af0ef6edc64539eecfba8f5656209
Anshumaan khare <94692766+Anshumaankhare123@users.noreply.github.com>
2023-11-09 00:02:43 +0530

    21

---
+++ JavaScript/String.js
@@ -0,0 +1,11 @@
+let text21 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let length = text21.length;
+console.log(length);
+let text22 = "Please visit Microsoft and Microsoft!";
+let newText = text22.replace("Microsoft", "W3Schools");
+console.log(newText);
+let text1 = "Hello World!";
+let text2 = text1.toUpperCase();
+let text3 = text1.toLowerCase();
+console.log(text2);
+console.log(text3);
