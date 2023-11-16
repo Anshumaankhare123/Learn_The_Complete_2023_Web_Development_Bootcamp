fc984ef0a42125313671c0516d733527e804190b
Anshumaan khare <94692766+Anshumaankhare123@users.noreply.github.com>
2023-11-16 12:46:15 +0530

    hua

---
+++ JavaScript/function.js
@@ -0,0 +1,11 @@
+function maths_for_you(a=100,b=100,c=200,d=1000){
+    let sum = a+b+c+d;
+    let avg = sum/4;
+    let multi = a*b*c*d;
+    let sub = a-b-c-d;
+    console.log("\n sum="+sum+"\n avg="+avg+"\n multi="+multi+"\n sub="+sub);
+    console.log(sum,avg,multi,sub);
+}
+
+
+maths_for_you();
