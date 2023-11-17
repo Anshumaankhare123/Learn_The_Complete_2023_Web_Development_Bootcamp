0235b4fe9a31794402761635d0fdd91dafdec788
Anshumaan khare <94692766+Anshumaankhare123@users.noreply.github.com>
2023-11-17 17:12:09 +0530

    d

---
+++ JavaScript/array_1.js
@@ -0,0 +1,11 @@
+const arr = [1,2,3,4,5,6,7,8,9,10];
+let size = arr.length;
+console.log("size="+size);
+console.log("arr ="+arr);
+for(let i=0;i< arr.length;i++){
+
+    console.log("value by index ="+arr[i]);
+}
+
+// this are the eg. of an array 
+// in this i use some methods
